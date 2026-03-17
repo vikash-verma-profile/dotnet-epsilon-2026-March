4e1781eb06e08d3a019a0b7d9f14018dca89bdc3
vikash-verma-profile <er.vikashverma@yahoo.com>
2026-03-17 09:58:02 +0530

    added day-2

---
+++ Day-2/labs/Day-2/Day-2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Day_2
+{
+
+    class Employee
+    {
+        //fields
+        int Id;
+        string EmployeeName;
+        //properties
+        //functions/methods
+        public Employee GetData()
+        {
+            Console.WriteLine("Please enter employee record:");
+            Employee employee = new Employee();
+            employee.Id = Convert.ToInt32(Console.ReadLine());
+            employee.EmployeeName = Console.ReadLine();
+            return employee;
+        }
+
+        public void Display(Employee[] employeeList)
+        {
+            foreach (var employee in employeeList)
+            {
+                Console.WriteLine($"{employee.Id}-{employee.EmployeeName}");
+            }
+        }
+        //constructors
+        //destructors
+        //indexers
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee[] employeeList = new Employee[3];
+            Employee emp = new Employee();
+            for (int i = 0; i < employeeList.Length; i++)
+            {
+                employeeList[i] = emp.GetData();
+            }
+
+            emp.Display(employeeList);
+
+        }
+    }
+}
