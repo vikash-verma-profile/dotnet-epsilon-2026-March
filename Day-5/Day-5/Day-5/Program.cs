ddb3a2ac17423972da764e175399d58948597485
vikash-verma-profile <er.vikashverma@yahoo.com>
2026-03-20 09:24:43 +0530

    added day-5

---
+++ Day-5/Day-5/Day-5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Day_5
+{
+
+    //single cast
+    public delegate void MyDelegate(string msg);
+    internal class Program
+    {
+        public static void Show(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static void Main(string[] args)
+        {
+            MyDelegate del = Show;
+            del("Vikash");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
