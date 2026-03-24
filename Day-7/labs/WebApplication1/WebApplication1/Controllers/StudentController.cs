1ba72c36d5910869764b6fe0048ff45926c65494
vikash-verma-profile <er.vikashverma@yahoo.com>
2026-03-24 11:29:50 +0530

    added dotet api

---
+++ Day-7/labs/WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok("Vikash");
+        }
+    }
+}
