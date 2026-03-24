8842e501d0651f1eb33f5cdc557466f1fb800246
vikash-verma-profile <er.vikashverma@yahoo.com>
2026-03-24 12:26:45 +0530

    added dotet api

--- Day-7/labs/WebApplication1/WebApplication1/Controllers/StudentController.cs
+++ Day-7/labs/WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -9,6 +10,23 @@ namespace WebApplication1.Controllers
     {
         [HttpGet]
         public IActionResult GetStudents()
+        {
+            return Ok("Vikash");
+        }
+
+        [HttpPost]
+        public IActionResult PostStudents([FromBody]Student student)
+        {
+            return Ok("Vikash");
+        }
+
+        [HttpPut("id")]
+        public IActionResult UpdateStudents([FromQuery] int id,[FromBody] Student student)
+        {
+            return Ok("Vikash");
+        }
+        [HttpDelete("id")]
+        public IActionResult DeleteStudent([FromQuery] int id)
         {
             return Ok("Vikash");
         }
--- Day-7/labs/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Day-7/labs/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+292264a7abcad2aa6b1c2bcbb1a27a46196e8876")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba72c36d5910869764b6fe0048ff45926c65494")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
